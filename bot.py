4a410105ffb689dcaa06114ca5982caebc56b869
chenghaoli36 <57383595+chenghaoli36@users.noreply.github.com>
2020-07-17 19:32:16 -0700

    added 2 small commands and made it work basically

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
 # bot.py
-
 import os
-
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='/.')
 
 client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
     print("its up")
 
-client.run(TOKEN)
+@bot.command()
+async def crash(ctx):
+    raise Exception("lol i crashed")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('hi')
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+bot.run(TOKEN)
