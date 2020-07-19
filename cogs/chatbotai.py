dfdb3ad796edbc11516d93b2b1d57a1e0e969625
chenghaoli36 <57383595+chenghaoli36@users.noreply.github.com>
2020-07-18 23:06:40 -0700

    tryed to fix some stuff and used differnt methods
    break again btw

--- cogs/chatbotai.py
+++ cogs/chatbotai.py
@@ -1,14 +1,18 @@
 # chatbotai.py
 
-# used for dialogflow api implementation. edited version of discord.py diaflow template https://github.com/SpectrixOfficial/discordpy-dialogflow-chatbot/blob/master/async/chatbot-ext.py
+# used for dialogflow api implementation
 
 import json, apiai, discord, asyncio, os
 from discord.ext import commands
 from dotenv import load_dotenv
-
 load_dotenv()
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
 
-ai = apiai.ApiAI(os.getenv("DIALOGFLOW_KEY"))
+DIALOGFLOW_PROJECT_ID = 'covid-ltbqsu'
+DIALOGFLOW_LANGUAGE_CODE = 'en-US'
+GOOGLE_APPLICATION_CREDENTIALS = 'covid-da1221d3a71e.json'
+SESSION_ID = 'current-user-id'
 class chat(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -21,13 +25,15 @@ class chat(commands.Cog):
                 else:
                     async with message.channel.typing():
                         user_message = message.content.replace(message.guild.me.mention,'') if message.guild else message.content
-                        request = ai.text_request()
-                        request.query = user_message
-                        response = json.loads(request.getresponse().read())
-                        result = response['result']
-                        action = result.get('action')
-                    print(response)
-                    await message.channel.send(f"{message.author.mention} {response['result']['fulfillment']['speech']}")
+                        session_client = dialogflow.SessionsClient()
+                        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+                        text_input = dialogflow.types.TextInput(text=user_message, language_code=DIALOGFLOW_LANGUAGE_CODE)
+                        query_input = dialogflow.types.QueryInput(text=text_input)
+                        try:
+                            response = session_client.detect_intent(session=session, query_input=query_input)
+                        except InvalidArgument:
+                            raise
+                    await message.channel.send(response)
             except Exception as error:
                 await message.channel.send("Make sure you didn't give too much or too little input and try agian.")
                 print(error)
